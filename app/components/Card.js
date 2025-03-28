eab137d97f361e0db63fdf8a2ea216c44c0d8dc0
Gelila-Fassil <gebeyehugelila46@gmail.com>
2025-03-28 07:21:47 -0700

    second section updated

--- app/components/Card.js
+++ app/components/Card.js
@@ -25,17 +25,17 @@ const Card = () => {
 
     </section>
 
-    <section className="flex">
+    <section className="flex  w-[90%] mx-auto mt-10 bg-white py-10 px-5 justify-between">
         <div>
             <div>
                 <h1 className="font-bold text-[16px] text-[#1E407B]">WHO WE ARE</h1>
             </div>
-            <h1>Fostering Global Unity for a Prosperous Ethiopia</h1>
-            <p>Ethiopian Diaspora Services (EDS) is a dedicated institution bridging Ethiopia with its global Diaspora. Our mission is to empower Ethiopians abroad to contribute to the nation's development while nurturing cultural ties and fostering unity. EDS embodies the shared responsibility of building a united and prosperous Ethiopia</p>
+            <h1 className="w-[40%] text-[32px] text-[#1384B7] font-medium ">Fostering Global Unity for a Prosperous Ethiopia</h1>
+            <p className="w-[40%] text-[16px] font-normal">Ethiopian Diaspora Services (EDS) is a dedicated institution bridging Ethiopia with its global Diaspora. Our mission is to empower Ethiopians abroad to contribute to the nation's development while nurturing cultural ties and fostering unity. EDS embodies the shared responsibility of building a united and prosperous Ethiopia</p>
 
         </div>
         <div>
-           {/* <Image src="/flag.webp" alt="logo"> </Image> */}
+           <Image src="/flag.webp" alt="logo" width={900} height={900} /> 
             
             </div>
     </section>
